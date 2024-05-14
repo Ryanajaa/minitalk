f89b4c9475232578103bee4c324a43c1e22d69a8
Jitpanu Arunotai <jarunota@z2t5c4.42bangkok.com>
2024-05-14 19:39:30 +0700

    up8

---
+++ include/minitalk.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jarunota <jarunota@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/26 17:22:37 by jarunota          #+#    #+#             */
+/*   Updated: 2024/05/14 16:55:53 by jarunota         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include "../libft/include/libft.h"
+# include "../libft/include/ft_printf.h"
+# include <signal.h>
+
+#endif
